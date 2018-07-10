c133c937e658e8c20cd6ad7102f9216d68185f8a
lizethquijia1997 <38613956+lizethquijia1997@users.noreply.github.com>
2018-07-10 09:03:55 -0500

    Add files via upload

---
+++ Prueba1/src/prueba1/Clientes.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Clientes {
+    
+    private String nombre;
+    private int cédula;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCédula() {
+        return cédula;
+    }
+
+    public void setCédula(int cédula) {
+        this.cédula = cédula;
+    }
+    
+    
+}
